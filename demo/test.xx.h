04abd877c36af38cb885d82498873528de577267
user <user@zt-2010271.ztgame.com>
2014-09-23 15:03:23 +0800

    越狱替换动态库测试demo

---
+++ demo/test.xx.h
@@ -0,0 +1,14 @@
+//
+//  test.xx.h
+//  iosdylib
+//
+//  Created by user on 14-9-23.
+//  Copyright (c) 2014年 wanglei. All rights reserved.
+//
+
+#ifndef iosdylib_test_xx_h
+#define iosdylib_test_xx_h
+
+
+
+#endif
